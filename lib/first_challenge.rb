006233b988a7aef0580d3c63d93112d26413cccd
David Fuentes <davidf0827@gmail.com>
2020-08-03 06:14:49 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,13 +1,16 @@
 def first_challenge
   epic_tragedy = {
-    :montague => {
+   :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-    }, 
-    :capulet => {
-      
-    }
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
    }
+}
 end
